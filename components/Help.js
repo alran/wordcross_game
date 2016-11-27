4eda36c5b5950772d54b06738452fc26115ba173
Alyssa Ransbury <aransbury@gmail.com>
2016-11-26 22:10:56 -0800

    Add help file

---
+++ components/Help.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+GLOBAL = require('../modules/Global.js');
+
+export default class Help extends Component {
+
+  render() {
+    return (
+      <View>
+        <Text>Help...</Text>
+      </View>
+    )
+  }
+}
+
+let styles = StyleSheet.create({
+});
