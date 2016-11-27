b3756674d45bb63bc9a51d69a759f48f00d71ce4
Alyssa Ransbury <aransbury@gmail.com>
2016-11-26 22:11:06 -0800

    Add profile file

---
+++ components/Profile.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+GLOBAL = require('../modules/Global.js');
+
+export default class Profile extends Component {
+
+  render() {
+    return (
+      <View>
+        <Text>Profile...</Text>
+      </View>
+    )
+  }
+}
+
+let styles = StyleSheet.create({
+});
